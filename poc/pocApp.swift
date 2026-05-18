72fd234df4dc7e3c2255018bc2fada6261053659
roooot <103732419+rooootdev@users.noreply.github.com>
2026-05-18 22:43:47 +0200

    Initial Commit

---
+++ poc/pocApp.swift
@@ -0,0 +1,17 @@
+//
+//  pocApp.swift
+//  poc
+//
+//  Created by ruter on 18.05.26.
+//
+
+import SwiftUI
+
+@main
+struct pocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
